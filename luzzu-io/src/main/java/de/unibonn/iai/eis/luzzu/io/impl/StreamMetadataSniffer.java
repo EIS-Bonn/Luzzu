894e88d0f5c2b2bca982363ca42b2cf75ea0cb7f
jerdeb <jerdebattista@gmail.com>
2014-08-06 17:27:24 +0200

    Cleaned up Stream Processor. Implemented StreamMetadataSniffer to extract quality metadata from assessed datasets and eventually be cached by the CacheManager

---
+++ luzzu-io/src/main/java/de/unibonn/iai/eis/luzzu/io/impl/StreamMetadataSniffer.java
@@ -0,0 +1,91 @@
+package de.unibonn.iai.eis.luzzu.io.impl;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.sparql.core.Quad;
+
+import de.unibonn.iai.eis.luzzu.cache.impl.TemporaryGraphMetadataCacheObject;
+import de.unibonn.iai.eis.luzzu.semantics.utilities.Commons;
+import de.unibonn.iai.eis.luzzu.semantics.vocabularies.DAQ;
+
+/**
+ * @author Jeremy Debattista
+ *
+ * This class acts as a helper class to the StreamProcessor
+ * to extract the available quality metadata from the assessed 
+ * dataset.
+ * The output of this builder will be cached for future
+ * use.
+ */
+class StreamMetadataSniffer {
+	
+	private TemporaryGraphMetadataCacheObject _temp = null;
+	private ConcurrentMap<String, Model> temporaryResources = new ConcurrentHashMap<String, Model>();
+	
+	/**
+	 * Takes quad and tries to build the quality metadata 
+	 * with the quad information available
+	 * 
+	 * The approach taken to build the temporary graph object
+	 * is a poor man's approach, keeping in mind the following:
+	 * 1) If the passed quad contains the triple "?x a DAQ.QualityGraph" then
+	 * be can create a new TemporaryGraphMetadataCacheObject with the subject
+	 * resource as the Graph URI
+	 * 2) Since a quad might contain the GRAPH URI in its fourth (context) element, 
+	 * then:
+	 *	a) first check if the TemporaryGraphMetadataCacheObject is created and
+	 *	if so, check if the context element (in the quad) is the same as defined 
+	 *	as the Graph URI of the java object. If these are the same, then it is 
+	 *  easy, as we just add the triples to the cached metadata.
+	 *  b) if the cached object is not created yet, then we create a temporary 
+	 *  jena model containing all resources of the same graph (these are known when
+	 *  quads are streamed) and then stored in a hashmap. This hashmap will be used
+	 *  for building the final cached object, pushing the corresponding model from the
+	 *  datastructure to the cached object.
+	 * 
+	 * @param quad
+	 */
+	protected void sniff(Quad quad){
+		if (quad.getGraph() != null){
+			if (this._temp != null && this._temp.getGraphURI() != null){
+				if (quad.getGraph().getURI().equals(this._temp.getGraphURI().getURI())){
+					this._temp.addTriplesToMetadata(quad);
+				}
+			} else {
+				if (this.temporaryResources.containsKey(quad.getGraph())){
+					Model m = this.temporaryResources.get(quad.getGraph());
+					m.add(Commons.asRDFNode(quad.getSubject()).asResource(), 
+							m.createProperty(quad.getPredicate().getURI()), 
+							Commons.asRDFNode(quad.getObject()));
+					this.temporaryResources.put(quad.getGraph().toString(), m);
+				} else {
+					Model m = ModelFactory.createDefaultModel();
+					m.add(Commons.asRDFNode(quad.getSubject()).asResource(), 
+							m.createProperty(quad.getPredicate().getURI()), 
+							Commons.asRDFNode(quad.getObject()));
+					this.temporaryResources.put(quad.getGraph().toString(), m);
+				}
+			}
+		}
+		if (quad.getObject().isURI()){
+			if (quad.getObject().getURI().equals(DAQ.QualityGraph.getURI())){
+				this._temp = new TemporaryGraphMetadataCacheObject(Commons.asRDFNode(quad.getObject()).asResource());
+			}
+		}
+	}
+	
+	/**
+	 * @return Returns the Object to be Cached
+	 */
+	protected TemporaryGraphMetadataCacheObject getCachingObject(){
+		if (_temp == null) return null;
+		if (this.temporaryResources.containsKey(_temp.getGraphURI().toString())){
+			_temp.addModelToMetadata(this.temporaryResources.get(_temp.getGraphURI()));
+		}
+		return _temp;
+	}
+	
+}
--- luzzu-io/src/main/java/de/unibonn/iai/eis/luzzu/io/impl/StreamProcessor.java
+++ luzzu-io/src/main/java/de/unibonn/iai/eis/luzzu/io/impl/StreamProcessor.java
@@ -1,9 +1,5 @@
 package de.unibonn.iai.eis.luzzu.io.impl;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
@@ -23,24 +19,29 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
-import com.hp.hpl.jena.rdf.model.impl.StatementImpl;
 import com.hp.hpl.jena.sparql.core.Quad;
-import com.hp.hpl.jena.vocabulary.RDF;
 
 import de.unibonn.iai.eis.luzzu.assessment.QualityMetric;
+import de.unibonn.iai.eis.luzzu.cache.CacheManager;
 import de.unibonn.iai.eis.luzzu.datatypes.Object2Quad;
 import de.unibonn.iai.eis.luzzu.exceptions.ProcessorNotInitialised;
 import de.unibonn.iai.eis.luzzu.io.IOProcessor;
-import de.unibonn.iai.eis.luzzu.semantics.utilities.Commons;
-import de.unibonn.iai.eis.luzzu.semantics.utilities.DAQHelper;
+import de.unibonn.iai.eis.luzzu.properties.PropertyManager;
 
+/**
+ * @author Jeremy Debattista
+ *
+ *	http://jena.apache.org/documentation/io/rdf-input.html
+ */
 public class StreamProcessor implements IOProcessor {
+	
+	private final CacheManager cacheMgr = CacheManager.getInstance();
+	private final String graphCacheName = PropertyManager.getInstance().getProperties("cache.properties").getProperty("GRAPH_METADATA_CACHE");
+	private Map<String, QualityMetric> metricInstances = new ConcurrentHashMap<String, QualityMetric>();
 
-	//http://jena.apache.org/documentation/io/rdf-input.html
 
-	protected String datasetURI; // this variable holds the filename or uri of datasets which needs to be processed
+	protected String datasetURI;
 	private PipedRDFIterator<?> iterator;
 	protected PipedRDFStream<?> rdfStream;
 	
@@ -48,13 +49,11 @@ public class StreamProcessor implements IOProcessor {
 
 	private boolean isInitalised = false;
 	
-	public StreamProcessor(String datasetURI){
-		this.datasetURI = datasetURI;
-	}
 	
 	public StreamProcessor(String datasetURI, boolean genQualityReport, Model configuration){
 		this.datasetURI = datasetURI;
 		// TODO: Complete setup
+		cacheMgr.createNewCache(graphCacheName, 50);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -75,21 +74,13 @@ public class StreamProcessor implements IOProcessor {
 		this.isInitalised = true;
 		
 		
-		// load Usecase specific metrics -- added for review year 1 prototype TODO: modify to be generic
-		try {
-			this.loadUsecaseSpecificMetrics("ebi");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
+		// load Usecase specific metrics 
 	}
 
 	public void startProcessing() throws ProcessorNotInitialised{
 		//TODO: if setUp is not called, throw an error
-		if(this.isInitalised == false) throw new ProcessorNotInitialised("Streaming will not start as processor has not been initalised");
+		if(this.isInitalised == false) throw new ProcessorNotInitialised("Streaming will not start as processor has not been initalised");		
+		StreamMetadataSniffer sniffer = new StreamMetadataSniffer();
 		
 		Runnable parser = new Runnable(){
 			public void run() {
@@ -97,19 +88,22 @@ public class StreamProcessor implements IOProcessor {
 			}
 		};
 		
-		executor.submit(parser);  // Start the parser on another thread
-	
+		executor.submit(parser); 
 
-		// loop which will go through the statements one by one
+		
 		while (this.iterator.hasNext()){
 			Object2Quad stmt = new Object2Quad(this.iterator.next());
+			sniffer.sniff(stmt.getStatement());
 			
 			for(String className : this.metricInstances.keySet()){
 				QualityMetric m = this.metricInstances.get(className);
 				m.compute(stmt.getStatement());
 			}
 		}
-		this.generateQualityGraph();
+		
+		if (sniffer.getCachingObject() != null){
+			cacheMgr.addToCache(graphCacheName, datasetURI, sniffer.getCachingObject());
+		}
 	}
 
 
@@ -120,10 +114,8 @@ public class StreamProcessor implements IOProcessor {
 			this.executor.shutdown();
 		}
 	}
-	
+		
 	///// TESTING /////
-	private Map<String, QualityMetric> metricInstances = new ConcurrentHashMap<String, QualityMetric>();
-	
 	@SuppressWarnings("unchecked")
 	private void loadUsecaseSpecificMetrics(String pilot) throws ClassNotFoundException, InstantiationException, IllegalAccessException{	
 		String filename = this.getClass().getClassLoader().getResource("metrics_initalisations/metrics.trig").toExternalForm();
@@ -147,83 +139,6 @@ public class StreamProcessor implements IOProcessor {
 		}	
 	}
 	
-	public void generateQualityGraph(){
-		HashMap<String,Resource> generatedURI = new HashMap<String,Resource>();
-		
-		Model m = ModelFactory.createDefaultModel();
-		
-			for(String className : this.metricInstances.keySet()){
-				QualityMetric met = this.metricInstances.get(className);
-				//System.out.println(met.getMetricURI());
-				List<Statement> daqTrips = met.toDAQTriples();
-				m.add(daqTrips);
-				
-				Resource dim = DAQHelper.getDimensionResource(met.getMetricURI());
-				if (!(generatedURI.containsKey(dim.getURI()))){
-					Resource dimURI = Commons.generateURI();
-					generatedURI.put(dim.getURI(), dimURI);
-				}
-				Statement dtype = new StatementImpl(generatedURI.get(dim.getURI()), RDF.type, dim);
-				Property p = m.createProperty(DAQHelper.getPropertyResource(met.getMetricURI()).getURI());
-				Statement dhasMet = new StatementImpl(generatedURI.get(dim.getURI()), p , daqTrips.get(0).getSubject());
-				m.add(dtype);
-				m.add(dhasMet);
-				
-				Resource cat = DAQHelper.getCategoryResource(met.getMetricURI());
-				if (!(generatedURI.containsKey(cat.getURI()))){
-					Resource catURI = Commons.generateURI();
-					generatedURI.put(cat.getURI(), catURI);
-				}
-				
-				Statement ctype = new StatementImpl(generatedURI.get(cat.getURI()), RDF.type, cat);
-				p = m.createProperty(DAQHelper.getPropertyResource(dim).getURI());
-				Statement chasDim = new StatementImpl(generatedURI.get(cat.getURI()), p, daqTrips.get(0).getSubject());
-				m.add(ctype);
-				m.add(chasDim);
-			}
-		
-		try {
-			String filename = "/Users/jeremy/Documents/Workspaces/eis/quality/src/main/resources/output/qg.trig";
-			RDFDataMgr.write(new FileOutputStream(filename,true), m, Lang.TRIG);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) throws ProcessorNotInitialised, ClassNotFoundException, InstantiationException, IllegalAccessException{
-		
-		for (int i=34; i<=44; i++){
-			if (i == 40) i++;
-			String filename = StreamProcessor.class.getClassLoader().getResource("testfiles/ebi/efo-2."+i+".rdf").toExternalForm();
-			
-			System.out.println("Processing efo-2."+i+".rdf");
-			process(filename);
-		}
-	}
-	
-	public static void process(String filename) throws ProcessorNotInitialised, ClassNotFoundException, InstantiationException, IllegalAccessException{
-		StreamProcessor sp = new StreamProcessor(filename);
-		sp.setUpProcess();
-		try {
-			sp.startProcessing();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		} finally {
-			sp.cleanUp();
-		}
-		System.out.println("finished");
-	}
 	
 	
-	
-//	public static void main(String[] args) throws ProcessorNotInitialised{
-//		String uri = "http://aksw.org/model/export/?m=http%3A%2F%2Faksw.org%2F&f=rdfxml";
-//		String filename = "D:\\Users\\jdebattist\\Desktop\\raw_infobox_properties_en.nq";
-//		
-//		StreamProcessor sp = new StreamProcessor(uri);
-//		sp.setUpProcess();
-//		sp.startProcessing();
-//		sp.cleanUp();
-//	}
 }
