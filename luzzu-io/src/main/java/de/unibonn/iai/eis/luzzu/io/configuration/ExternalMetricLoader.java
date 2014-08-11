545fb3e521f6b7563b2d8867cc0753843d751619
jerdeb <jerdebattista@gmail.com>
2014-08-11 19:18:59 +0200

    added external metric loader and external folder to add external jars

---
+++ luzzu-io/src/main/java/de/unibonn/iai/eis/luzzu/io/configuration/ExternalMetricLoader.java
@@ -0,0 +1,102 @@
+package de.unibonn.iai.eis.luzzu.io.configuration;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.NodeIterator;
+
+import de.unibonn.iai.eis.luzzu.assessment.QualityMetric;
+import de.unibonn.iai.eis.luzzu.semantics.vocabularies.LMI;
+
+
+/**
+ * @author Jeremy Debattista
+ * 
+ * The class handles the loading of External Quality Metrics.
+ * 
+ */
+public class ExternalMetricLoader {
+
+	final static Logger logger = LoggerFactory.getLogger(ExternalMetricLoader.class);
+	private static ExternalMetricLoader instance = null;
+	private static ConcurrentMap<File,List<String>> metricsInFile = new ConcurrentHashMap<File,List<String>>(); // Jar File, List of Metrics
+	
+	private static FileFilter jarFilter = new FileFilter() {
+		public boolean accept(File file) {
+			if (file.getName().endsWith(".jar")) {
+				return true;
+			}
+			return false;
+		}
+	};
+	
+	protected ExternalMetricLoader() {}
+	
+	public static ExternalMetricLoader getInstance(){
+		if (instance == null) {
+			logger.info("Initialising and verifying external metrics.");
+			instance = new ExternalMetricLoader();
+			loadMetrics();	
+		}
+		
+		return instance;
+	}
+	
+	private static void loadMetrics(){
+		File externalsFolder = new File("externals/");
+		File[] listOfFiles = externalsFolder.listFiles();
+		
+		for(File metrics : listOfFiles){
+			File jarFile = metrics.listFiles(jarFilter)[0];
+			metricsInFile.putIfAbsent(jarFile, new ArrayList<String>());
+			logger.info("Loading metrics from : {} ", jarFile.toPath());
+			
+			Model m = ModelFactory.createDefaultModel();
+			m.read(metrics+"//metrics.trig");
+			
+			
+			NodeIterator res = m.listObjectsOfProperty(LMI.javaPackageName);
+			while (res.hasNext()){
+				String javaMetric = res.next().toString();
+				metricsInFile.get(jarFile).add(javaMetric);
+				logger.info("Metric : {} ", javaMetric);
+			}
+		}
+	}
+	
+	@SuppressWarnings({ "resource", "unchecked" })
+	public Map<String, Class<? extends QualityMetric>> getQualityMetricClasses() throws InstantiationException, IllegalAccessException, MalformedURLException, ClassNotFoundException{
+		if (instance == null) getInstance();
+		Map<String, Class<? extends QualityMetric>> clazzes = new HashMap<String,Class<? extends QualityMetric>>();
+		
+		for(File jar : metricsInFile.keySet()){
+			List<String> metrics = metricsInFile.get(jar);
+			
+			URL jarUrl = new URL("file:" + jar.getAbsolutePath());
+			URLClassLoader cl = new URLClassLoader(new URL[] {jarUrl}, ExternalMetricLoader.class.getClassLoader());
+			
+			for(String m : metrics){
+				logger.info("Creating class for metric : {} ", m);
+				Class<? extends QualityMetric> clazz = (Class<? extends QualityMetric>) cl.loadClass(m);
+				clazzes.put(m, clazz);
+			}
+		}
+		
+		return clazzes;
+	}
+	
+}
