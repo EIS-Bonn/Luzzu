6fa2f689611618367039a1d1a1f1e0a3c0f3bce1
jerdeb <jerdebattista@gmail.com>
2014-11-04 22:29:58 +0100

    updated metrics

--- luzzu-evaluation/src/main/java/de/unibonn/iai/eis/luzzu/evaluation/Main.java
+++ luzzu-evaluation/src/main/java/de/unibonn/iai/eis/luzzu/evaluation/Main.java
@@ -3,14 +3,17 @@ package de.unibonn.iai.eis.luzzu.evaluation;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 
 import de.unibonn.iai.eis.luzzu.evaluation.settings.DataGenerator;
 import de.unibonn.iai.eis.luzzu.evaluation.settings.EvaluationCase;
 import de.unibonn.iai.eis.luzzu.exceptions.ProcessorNotInitialised;
-import de.unibonn.iai.eis.luzzu.io.impl.StreamProcessor;
+import de.unibonn.iai.eis.luzzu.io.impl.SparkStreamProcessorObserver;
+import de.unibonn.iai.eis.luzzu.io.impl.StreamProcessorObserver;
 
 public class Main {
 	
@@ -18,176 +21,70 @@ public class Main {
 	private static long tEnd;
 	
 	private static List<EvaluationCase> eCases = new ArrayList<EvaluationCase>();
-	//private static int metrics[] = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13}; 
 	private static int scalefactor[] = new int[]{24,57,128,199,256,666,1369,2089,2785,28453,70812,141000,284826};
-	protected static String fileNames[] = new String[]{
-		"ec_0_9861_091545.dat",
-	    "ec_0_25730_091548.dat",
-		"ec_0_51091_091555.dat",
-		"ec_0_76010_091604.dat",
-		"ec_0_99032_091614.dat",
-		"ec_0_251428_091636.dat",
-		"ec_0_503996_091717.dat",
-		"ec_0_754624_091817.dat",
-		"ec_0_999972_091935.dat",
-		"ec_0_10011487_093209.dat",
-		"ec_0_24835339_100613.dat",
-		"ec_0_49013816_114828.dat",
-		"ec_0_98957876_144830.dat"
-	};
-	
-	private static int eCaseNumber = 14;
-	
-	private static void setUp(int metrics){
-		//Evaluation Case #1 - approximately 10K triples (BSBM scale factor 24)
-		Integer triples = DataGenerator.generateData(24, "bsbm-24");
-		EvaluationCase _case = new EvaluationCase("Case 1 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-24.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #2 - approximately 25K triples (BSBM scale factor 57)
-		triples = DataGenerator.generateData(57, "bsbm-57");
-		_case = new EvaluationCase("Case 2 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-57.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #3 - approximately 50K triples (BSBM scale factor 128)
-		triples = DataGenerator.generateData(128, "bsbm-128");
-		_case = new EvaluationCase("Case 3 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-128.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #4 - approximately 75K triples (BSBM scale factor 199)
-		triples = DataGenerator.generateData(199, "bsbm-199");
-		_case = new EvaluationCase("Case 4 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-199.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #5 - approximately 100K triples (BSBM scale factor 256)
-		triples = DataGenerator.generateData(256, "bsbm-256");
-		_case = new EvaluationCase("Case 5 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-256.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #6 - approximately 250K triples (BSBM scale factor 666)
-		triples = DataGenerator.generateData(666, "bsbm-666");
-		_case = new EvaluationCase("Case 6 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-666.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #7 - approximately 500K triples (BSBM scale factor 1369)
-		triples = DataGenerator.generateData(1369, "bsbm-1369");
-		_case = new EvaluationCase("Case 7 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-1369.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #8 - approximately 750K triples (BSBM scale factor 2089)
-		triples = DataGenerator.generateData(2089, "bsbm-2089");
-		_case = new EvaluationCase("Case 8 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-2089.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #9 - approximately 1M triples (BSBM scale factor 2785)
-		triples = DataGenerator.generateData(2785, "bsbm-2785");
-		_case = new EvaluationCase("Case 9 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-2785.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #10 - approximately 10M triples (BSBM scale factor 28453)
-		triples = DataGenerator.generateData(28453, "bsbm-28453");
-		_case = new EvaluationCase("Case 10 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-28453.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #11 - approximately 25M triples (BSBM scale factor 70812)
-		triples = DataGenerator.generateData(70812, "bsbm-70812");
-		_case = new EvaluationCase("Case 11 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-70812.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #12 - approximately 50M triples (BSBM scale factor 141000)
-		triples = DataGenerator.generateData(141000, "bsbm-141000");
-		_case = new EvaluationCase("Case 12 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-141000.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
-		//Evaluation Case #13 - approximately 100M triples (BSBM scale factor 284826)
-		triples = DataGenerator.generateData(284826, "bsbm-284826");
-		_case = new EvaluationCase("Case 13 - Metrics Initialised : " + metrics + "; Dataset " + triples + " triples", new File("bsbm-284826.nt").getPath(), metrics);
-		_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
-		_case.setTotalTriples(triples);
-		eCases.add(_case);
-		
+	private static Map<Integer, Integer> generatedTriples = new HashMap<Integer,Integer>();
+	static{
+		generatedTriples.put(24, 9861);
+		generatedTriples.put(57, 25730);
+		generatedTriples.put(128, 51091);
+		generatedTriples.put(199, 76010);
+		generatedTriples.put(256, 99032);
+		generatedTriples.put(666, 251428);
+		generatedTriples.put(1369, 503996);
+		generatedTriples.put(2089, 754624);
+		generatedTriples.put(2785, 999972);
 	}
 	
-	private static void setUp2(int metrics) throws ClassNotFoundException, IOException{
-		for (String str : fileNames){
-			EvaluationCase _case = new EvaluationCase("evaluation_cases/"+str, metrics);
-			_case.setCaseName("Case "+ (eCaseNumber) + " -  Metrics Initialised : " + metrics + "; Dataset " + _case.getTotalTriples() + " triples");
-			_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with "+ metrics +" initialised metrics and a dataset with "+  _case.getTotalTriples()  + " triples");
+	private static boolean firstTimeGeneration = false;
+
+	private static void setUp(int metrics) throws ClassNotFoundException, IOException{
+		for (int sf : scalefactor){
+			System.out.println("Generating Case for scale factor "+ sf + " and " + metrics + " metrics");
+			Integer triples;
+			if (firstTimeGeneration){
+				System.out.println("Generating Triples");
+				triples = DataGenerator.generateData(sf, "bsbm-"+sf);
+				generatedTriples.put(sf, triples);
+			} else {
+				triples = generatedTriples.get(sf);
+			}
+			EvaluationCase _case = new EvaluationCase("Metrics Initialised : " + metrics + "; Dataset: " + triples + " triples; Scale Factor: "+sf, new File("bsbm-"+sf+".nt").getPath(), metrics);
+			_case.setCaseDescription("In this Evaluation Case, we evaluated the stream processor with no initialised metrics and a dataset with "+ triples + " triples");
+			_case.setTotalTriples(triples);
 			eCases.add(_case);
-			eCaseNumber++;
 		}
-	}
-	
-	private static void generateData(){
-		DataGenerator.generateData(24, "bsbm-24");
-		DataGenerator.generateData(57, "bsbm-57");
-		 DataGenerator.generateData(128, "bsbm-128");
-		 DataGenerator.generateData(199, "bsbm-199");
-		 DataGenerator.generateData(256, "bsbm-256");
-		 DataGenerator.generateData(666, "bsbm-666");
-		 DataGenerator.generateData(1369, "bsbm-1369");
-		 DataGenerator.generateData(2089, "bsbm-2089");
-		 DataGenerator.generateData(2785, "bsbm-2785");
-		 DataGenerator.generateData(28453, "bsbm-28453");
-		 DataGenerator.generateData(70812, "bsbm-70812");
-		 DataGenerator.generateData(141000, "bsbm-141000");
-		 DataGenerator.generateData(284826, "bsbm-284826");
+		firstTimeGeneration = false;
 	}
 	
 	
 	public static void main (String [] args) throws ProcessorNotInitialised, IOException, ClassNotFoundException{
-		System.out.println("generate data");
-		generateData();
-		
 		//create csv file
 		File csv = new File("benchmark.csv");
 		
-		if (!(csv.isFile())) csv.createNewFile();
+		if (!(csv.isFile())) {
+			csv.createNewFile();
+			String header = "triples,metrics,average time(s),min time(s),max time(s)";
+			FileUtils.write(csv, header, true);
+			FileUtils.write(csv, System.getProperty("line.separator"), true);
+		}
 		
-		String header = "triples,metrics,average time(s),min time(s),max time(s)";
-		FileUtils.write(csv, header, true);
-		FileUtils.write(csv, System.getProperty("line.separator"), true);
-		
-		for (int metric = 0; metric <=13 ; metric++ ){
+//		firstTimeGeneration = true;
+		for (int metric = 0; metric <= 9 ; metric++ ){
 			eCases = new ArrayList<EvaluationCase>();
-			setUp2(metric);
-			int iterations = 3;
+			setUp(metric);
+			int iterations = 4;
 			
 			for (EvaluationCase eCase : eCases){
 				System.out.println("Evaluating " + eCase.getCaseName());
 				System.out.println("=================================");
-				
-				StreamProcessor p = new StreamProcessor(eCase.getDatasetURI(), false, eCase.getMetricConfiguration()); 		// initiate stream processor
-				
+							
 				//Run benchmark for 10 iterations + 3 cold starts
-				for(int i = -1; i <= iterations; i++){
+				for(int i = -2; i <= iterations; i++){
+					SparkStreamProcessorObserver p = new SparkStreamProcessorObserver(eCase.getDatasetURI(), false, eCase.getMetricConfiguration()); 		// initiate stream processor
+
 					// setup processor
 					p.setUpProcess();
-	
+					
 					if (i >= 1){
 						//process
 						tStart = System.currentTimeMillis();
@@ -198,16 +95,17 @@ public class Main {
 						System.out.println("Iteration # : " + i + " - " + (difference / 1000.0));
 					} else {
 						p.startProcessing();
+						System.out.println("Cold Run.."+ (i + 2));
 					}
+					
+					//cleanup
+					p.cleanUp();
 				}
 				
 				FileUtils.write(csv, eCase.toString(), true);
 				FileUtils.write(csv, System.getProperty("line.separator"), true);
 				
 				eCase.storeEvaluationCase();
-				
-				//cleanup
-				p.cleanUp();
 			}
 		}
 	}
--- luzzu-evaluation/src/main/java/de/unibonn/iai/eis/luzzu/evaluation/settings/ModelConfiguration.java
+++ luzzu-evaluation/src/main/java/de/unibonn/iai/eis/luzzu/evaluation/settings/ModelConfiguration.java
@@ -14,21 +14,17 @@ public class ModelConfiguration {
 	public static Model getModelConfiguration(int initialisedMetrics){
 		List<String> metricList = new ArrayList<String>();
 		switch (initialisedMetrics){
-		//	case 13 : metricList.add("eu.diachron.qualitymetrics.intrinsic.consistency.HomogeneousDatatypes"); 
-			case 12 : metricList.add("eu.diachron.qualitymetrics.accessibility.interlinking.InterlinkDetectionMetric");
-			case 11 : metricList.add("eu.diachron.qualitymetrics.accessibility.licensing.MachineReadableLicense");
-			case 10 : metricList.add("eu.diachron.qualitymetrics.accessibility.interlinking.LinkExternalDataProviders");
-			case 9 : metricList.add("eu.diachron.qualitymetrics.accessibility.availability.DereferencibilityForwardLinks");
-			case 8 : metricList.add("eu.diachron.qualitymetrics.accessibility.availability.DereferencibilityBackLinks");
-			case 7 : metricList.add("eu.diachron.qualitymetrics.accessibility.licensing.HumanReadableLicense");
+			case 9 : metricList.add("eu.diachron.qualitymetrics.intrinsic.conciseness.ExtensionalConcisenessBloom");
+			case 8 : metricList.add("eu.diachron.qualitymetrics.intrinsic.accuracy.MalformedDatatypeLiterals");
+			case 7 : metricList.add("eu.diachron.qualitymetrics.representational.conciseness.ShortURIs");
 			case 6 : metricList.add("eu.diachron.qualitymetrics.representational.understandability.HumanReadableLabelling");
-			case 5 : metricList.add("eu.diachron.qualitymetrics.representational.conciseness.ShortURIs");
-			case 4 : metricList.add("eu.diachron.qualitymetrics.intrinsic.accuracy.MalformedDatatypeLiterals");
-			case 3 : metricList.add("eu.diachron.qualitymetrics.intrinsic.conciseness.ExtensionalConciseness");
-			case 2 : metricList.add("eu.diachron.qualitymetrics.intrinsic.conciseness.DuplicateInstance");
-			case 1 : metricList.add("eu.diachron.qualitymetrics.contextual.amountofdata.AmountOfTriples"); break;
+			case 5 : metricList.add("eu.diachron.qualitymetrics.accessibility.interlinking.LinkExternalDataProviders");
+			case 4 : metricList.add("eu.diachron.qualitymetrics.accessibility.availability.DereferencibilityBackLinks");
+			case 3 : metricList.add("eu.diachron.qualitymetrics.accessibility.licensing.MachineReadableLicense");
+			case 2	: metricList.add("eu.diachron.qualitymetrics.accessibility.licensing.HumanReadableLicense");
+			case 1 : metricList.add("eu.diachron.qualitymetrics.accessibility.availability.DereferencibilityForwardLinks");break;
 			case 0 : break;
-			default : break;
+			default : break;		
 		}
 		
 		Model m = ModelFactory.createDefaultModel();
