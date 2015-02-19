d37f1c6c189ab992bb70892afca676602e5a7604
jerdeb <jerdebattista@gmail.com>
2015-02-19 12:22:06 +0100

    added two methods for defining if metric is an estimation and for tracking provenance information

--- luzzu-assessment/src/main/java/de/unibonn/iai/eis/luzzu/assessment/QualityMetric.java
+++ luzzu-assessment/src/main/java/de/unibonn/iai/eis/luzzu/assessment/QualityMetric.java
@@ -32,4 +32,20 @@ public interface QualityMetric {
 	 * @return returns a typed ProblemList which will be used to create a "quality report" of the metric.
 	 */
 	ProblemList<?> getQualityProblems();
+	
+	
+	/**
+	 * @return returns true if the assessed metric returns an estimate result due to its probabilistic assessment technique (e.g. bloom filters)
+	 */
+	boolean isEstimate();
+	
+	
+	/**
+	 * An agent is required to keep provenance track.  We encourage the definition of an
+	 * agent which can be accessible online, or as part of the metrics vocabulary.
+	 * 
+	 * @return returns the Agent URI that assessed the metric's observation
+	 */
+	Resource getAgentURI();
+	
 }
