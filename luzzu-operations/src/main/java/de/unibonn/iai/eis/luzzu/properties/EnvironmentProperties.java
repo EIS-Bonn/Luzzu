1313bde76b1026329e8285e5957d519eeec10134
jerdeb <jerdebattista@gmail.com>
2015-03-19 12:03:41 +0100

    added baseURI methods

--- luzzu-operations/src/main/java/de/unibonn/iai/eis/luzzu/properties/EnvironmentProperties.java
+++ luzzu-operations/src/main/java/de/unibonn/iai/eis/luzzu/properties/EnvironmentProperties.java
@@ -26,6 +26,26 @@ public class EnvironmentProperties {
 		if (PropertyManager.getInstance().environmentVars.containsKey("datasetURI")){
 			return PropertyManager.getInstance().environmentVars.get("datasetURI");
 		} else {
+			throw new IllegalStateException("Dataset is not Set");
+		}
+	}
+	
+	/**
+	 * Sets the dataset URI
+	 * @param datasetURI
+	 */
+	public void setDatasetURI(String datasetURI){
+		PropertyManager.getInstance().environmentVars.put("datasetURI", datasetURI);
+	}
+	
+	/**
+	 * Returns the base URI being processed.
+	 * @throws Exception if the process is not initialised and dataset is not known
+	 */
+	public String getBaseURI() throws IllegalStateException {
+		if (PropertyManager.getInstance().environmentVars.containsKey("baseURI")){
+			return PropertyManager.getInstance().environmentVars.get("baseURI");
+		} else {
 			throw new IllegalStateException("Processor is not initialised yet");
 		}
 	}
