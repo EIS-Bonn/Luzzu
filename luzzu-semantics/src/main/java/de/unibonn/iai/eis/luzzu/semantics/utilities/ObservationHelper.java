b480ec3c2548f9c9704cb4844942dcdb72bd8602
jerdeb <jerdebattista@gmail.com>
2015-06-25 10:33:38 +0200

    added helper class for observations

---
+++ luzzu-semantics/src/main/java/de/unibonn/iai/eis/luzzu/semantics/utilities/ObservationHelper.java
@@ -0,0 +1,55 @@
+package de.unibonn.iai.eis.luzzu.semantics.utilities;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.NodeIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+import de.unibonn.iai.eis.luzzu.semantics.datatypes.Observation;
+import de.unibonn.iai.eis.luzzu.semantics.vocabularies.DAQ;
+
+public class ObservationHelper {
+	
+	public static List<Observation> extractObservations(Model qualityMD, Resource metric){
+		List<Observation> lst = new ArrayList<Observation>(); 
+		
+		NodeIterator iter = qualityMD.listObjectsOfProperty(metric, DAQ.hasObservation);
+		while(iter.hasNext()){
+			Resource res = iter.next().asResource();
+			
+			//get datetime
+			Date date = null;
+			try {
+				date = toDateFormat(qualityMD.listObjectsOfProperty(res, qualityMD.createProperty("http://purl.org/linked-data/sdmx/2009/dimension#timePeriod")).next().asLiteral().getValue().toString());
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			//get value
+			Double value = qualityMD.listObjectsOfProperty(res, DAQ.value).next().asLiteral().getDouble();
+			
+			Observation obs = new Observation(res, date, value, null);
+			lst.add(obs);
+		}
+		
+		return lst;
+	}
+	
+	private static Date toDateFormat(String date) throws ParseException{
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'");
+		return sdf.parse(date);
+	}
+	
+	public static Observation getLatestObservation(List<Observation> observations){
+		Collections.sort(observations);
+		Collections.reverse(observations);
+		return observations.get(0);
+	}
+}
