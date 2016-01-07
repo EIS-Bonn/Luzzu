5164db1c2643a257a0065b2fe03795c723141665
jerdeb <jerdebattista@gmail.com>
2016-01-06 16:15:10 -1000

    added large stream processor

--- luzzu-io/src/main/java/de/unibonn/iai/eis/luzzu/io/ProcessorController.java
+++ luzzu-io/src/main/java/de/unibonn/iai/eis/luzzu/io/ProcessorController.java
@@ -6,6 +6,7 @@ import java.io.RandomAccessFile;
 
 import com.hp.hpl.jena.rdf.model.Model;
 
+import de.unibonn.iai.eis.luzzu.io.impl.LargeStreamProcessor;
 import de.unibonn.iai.eis.luzzu.io.impl.MemoryProcessor;
 import de.unibonn.iai.eis.luzzu.io.impl.StreamProcessor;
 
@@ -52,8 +53,10 @@ public class ProcessorController {
 		
 		if (length <= ((double) freeMemory * (1.0 / 100.0))) // if it fits in 1/100 of the free memory, then we use a memory processor
 			return new MemoryProcessor(baseURI,datasetURI,genQualityReport,modelConfig);
-		else 
+		else if (length <= ((double) freeMemory * (1.0 / 2.0))) // if it fits in 1/2 of the free memory, then we use a normal stream processor
 			return new StreamProcessor(baseURI,datasetURI,genQualityReport,modelConfig);
+		else
+			return new LargeStreamProcessor(baseURI,datasetURI,genQualityReport,modelConfig); //e.g for dbpedia etc
 	}
 
 }
