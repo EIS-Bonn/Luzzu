4fe8e1b800a11b99c54e83dd4fca77d010176f14
Jeremy Debattista <jerdebattista@gmail.com>
2017-09-14 14:31:43 +0100

    Update docker.sh

--- setup/docker.sh
+++ setup/docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH_INSTALLATION='/Luzzu' # the path where Luzzu and the Quality Metric repository should be installed
+PATH_INSTALLATION='/usr/bin' # the path where Luzzu and the Quality Metric repository should be installed
 SET_WEBAPP=true # true if you want to access the web application from localhost; else false
 WEBAPP_LOCATION='/var/www/html' # localhost application location
 INSTALL_EXAMPLES=true # true if you want to install the example metrics in luzzu
